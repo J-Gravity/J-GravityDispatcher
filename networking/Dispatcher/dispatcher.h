563de81ba9c7d538394d91a1906102df18840ff3
Can Yildirim <cyildiri@student.42.us.org>
2017-05-14 20:23:06 -0700

    added prototype for the work unit serializer

--- networking/Dispatcher/dispatcher.h
+++ networking/Dispatcher/dispatcher.h
@@ -245,11 +245,18 @@ int 		request_cache_dump(t_dispatcher *dispatcher, t_worker *worker);
 int			send_work_unit(t_dispatcher *dispatcher, t_worker *worker);
 
 /*
+*	Serializes the work_unit struct and stores it in the message struct
+*		@param	work_unit	The work unit that will be stored in the msg
+*		@return message struct conataining the serialized work unit
+*/
+t_msg		serialize_work_unit(t_work_unit *work_unit);
+
+/*
 *	Parse the data of a worker message and write it to t_work_unit struct
 *		@param	msg	The message from the worker that contains a complete
 					work unit
 */
-t_work_unit deserialize_work_unit(t_msg msg);
+t_work_unit	deserialize_work_unit(t_msg msg);
 
 /*
 *	Handles the request for broadacasting a super particle to all the
