f75333c265aaf6bd4099a6e1a73d508bac2cd294
Sean Smith <ssmith@student.42.us.org>
2017-05-14 22:14:46 -0700

    send_work_unit

--- networking/Dispatcher/send_work_unit.c
+++ networking/Dispatcher/send_work_unit.c
@@ -6,7 +6,7 @@
 /*   By: scollet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/09 22:42:08 by scollet           #+#    #+#             */
-/*   Updated: 2017/05/14 21:55:09 by ssmith           ###   ########.fr       */
+/*   Updated: 2017/05/14 22:14:31 by ssmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int   send_work_unit(t_worker *worker, t_work_unit work_unit)
 	t_msg	msg;
 
 	msg = serialize_work_unit(work_unit);
-	send_worker_msg(worker, 1, msg->size, msg->data);
+	msg.id = 1;
+	send_worker_msg(worker, msg);
 	return (0);
 }
