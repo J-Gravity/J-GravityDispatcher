1de2fe6c5233ee9da68f3840ddcb5da254fecea5
Sean Smith <ssmith@student.42.us.org>
2017-05-17 17:17:51 -0700

    updated deserialize_work_unit func

--- networking/Worker/deserialize_work_unit.c
+++ networking/Worker/deserialize_work_unit.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "dispatcher.h"
-
 int		btoi(char *str)
 {
 	int				ret;
@@ -30,60 +29,45 @@ float	btof(char *str)
 	return (ret);
 }
 
-t_work_unit	*deserialize_work_unit(t_msg msg)
+cl_float4	btoclf(char *str)
+{
+	cl_float4	ret;
+
+	for (unsigned int i = 0; i < sizeof(cl_float4); i++)
+		((char *)(&ret))[i] = str[i];
+	return (ret);
+}
+
+t_workunit	*deserialize_workunit(t_msg msg)
 {
 	int			i;
-	t_work_unit	*work_unit;
+	t_workunit	*workunit;
 
 	i = 0;
-	work_unit = malloc(sizeof(t_work_unit));
-	work_unit->compute_class = btoi(msg.data += 4);
-	work_unit->cell.body_count = btoi(msg.data += 4);
-	work_unit->cell.contained_bodies = (t_body **)malloc(sizeof(t_body *) * work_unit->cell.body_count);
-	for (int i = 0; i < work_unit->cell.body_count; i++)
+	workunit = malloc(sizeof(t_workunit));
+	workunit->id = btoi(msg.data);
+	workunit->localcount = btoi(msg.data += 4);
+	workunit->local_bodies = (t_body *)malloc(sizeof(t_body) * workunit->localcount);
+	for (int i = 0; i < workunit->localcount; i++)
 	{
-		work_unit->cell.contained_bodies[i] = (t_body *)malloc(sizeof(t_body));
-		work_unit->cell.contained_bodies[i]->position.x = btof(msg.data += 4);
-		work_unit->cell.contained_bodies[i]->position.y = btof(msg.data += 4);
-		work_unit->cell.contained_bodies[i]->position.z = btof(msg.data += 4);
-		work_unit->cell.contained_bodies[i]->velocity.x = btof(msg.data += 4);
-		work_unit->cell.contained_bodies[i]->velocity.y = btof(msg.data += 4);
-		work_unit->cell.contained_bodies[i]->velocity.z = btof(msg.data += 4);
-		work_unit->cell.contained_bodies[i]->mass = btof(msg.data += 4);
+		workunit->local_bodies[i].position.x = btof(msg.data += 4);
+		workunit->local_bodies[i].position.y = btof(msg.data += 4);
+		workunit->local_bodies[i].position.z = btof(msg.data += 4);
+		workunit->local_bodies[i].velocity.x = btof(msg.data += 4);
+		workunit->local_bodies[i].velocity.y = btof(msg.data += 4);
+		workunit->local_bodies[i].velocity.z = btof(msg.data += 4);
 	}
-	work_unit->cell.cell_as_body.position.x = btof(msg.data += 4);
-	work_unit->cell.cell_as_body.position.y = btof(msg.data += 4);
-	work_unit->cell.cell_as_body.position.z = btof(msg.data += 4);
-	work_unit->cell.cell_as_body.velocity.x = btof(msg.data += 4);
-	work_unit->cell.cell_as_body.velocity.y = btof(msg.data += 4);
-	work_unit->cell.cell_as_body.velocity.z = btof(msg.data += 4);
-	work_unit->cell.cell_as_body.mass = btof(msg.data += 4);
-	
-	work_unit->adjoining_cells_cnt = btoi(msg.data += 4);
-	work_unit->adjoining_cells = (t_cell *)malloc(sizeof(t_cell) * work_unit->adjoining_cells_cnt);
-	for (int i = 0; i < work_unit->adjoining_cells_cnt; i++)
+	workunit->neighborcount = btoi(msg.data += 4);
+	workunit->neighborhood = (t_body *)malloc(sizeof(t_body) * workunit->neighborcount);
+	for (int i = 0; i < workunit->neighborcount; i++)
 	{
-		work_unit->adjoining_cells[i].body_count = btoi(msg.data += 4);
-		work_unit->adjoining_cells[i].contained_bodies =
-			(t_body **)malloc(sizeof(t_body *) * work_unit->adjoining_cells[i].body_count);
-		for (int x = 0; x < work_unit->adjoining_cells[i].body_count; x++)
-		{
-			work_unit->adjoining_cells[i].contained_bodies[x] = (t_body *)malloc(sizeof(t_body));
-			work_unit->adjoining_cells[i].contained_bodies[x]->position.x = btof(msg.data += 4);
-			work_unit->adjoining_cells[i].contained_bodies[x]->position.y = btof(msg.data += 4);
-			work_unit->adjoining_cells[i].contained_bodies[x]->position.z = btof(msg.data += 4);
-			work_unit->adjoining_cells[i].contained_bodies[x]->velocity.x = btof(msg.data += 4);
-			work_unit->adjoining_cells[i].contained_bodies[x]->velocity.y = btof(msg.data += 4);
-			work_unit->adjoining_cells[i].contained_bodies[x]->velocity.z = btof(msg.data += 4);
-			work_unit->adjoining_cells[i].contained_bodies[x]->mass = btof(msg.data += 4);
-		}
-		work_unit->adjoining_cells[i].cell_as_body.position.x = btof(msg.data += 4);
-		work_unit->adjoining_cells[i].cell_as_body.position.y = btof(msg.data += 4);
-		work_unit->adjoining_cells[i].cell_as_body.position.z = btof(msg.data += 4);
-		work_unit->adjoining_cells[i].cell_as_body.velocity.x = btof(msg.data += 4);
-		work_unit->adjoining_cells[i].cell_as_body.velocity.y = btof(msg.data += 4);
-		work_unit->adjoining_cells[i].cell_as_body.velocity.z = btof(msg.data += 4);
-		work_unit->adjoining_cells[i].cell_as_body.mass = btof(msg.data += 4);
+		workunit->neighborhood[i].position.x = btof(msg.data += 4);
+		workunit->neighborhood[i].position.y = btof(msg.data += 4);
+		workunit->neighborhood[i].position.z = btof(msg.data += 4);
+		workunit->neighborhood[i].velocity.x = btof(msg.data += 4);
+		workunit->neighborhood[i].velocity.y = btof(msg.data += 4);
+		workunit->neighborhood[i].velocity.z = btof(msg.data += 4);
 	}
-	return (work_unit);
+	workunit->force_bias = btoclf(msg.data += 4);
+	return (workunit);
 }
