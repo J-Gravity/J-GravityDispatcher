ce9ea315ea290dd32d9b4a7cbe1e377fb255fc82
Sean Smith <ssmith@student.42.us.org>
2017-05-18 23:43:35 -0700

    trying to fix double worker issue

--- networking/Dispatcher/connect_workers.c
+++ networking/Dispatcher/connect_workers.c
@@ -6,7 +6,7 @@
 /*   By: scollet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/08 21:10:00 by scollet           #+#    #+#             */
-/*   Updated: 2017/05/18 19:57:37 by ssmith           ###   ########.fr       */
+/*   Updated: 2017/05/18 23:35:08 by ssmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 
 void	*connect_worker_thread(void *param)
 {
+	t_lst					*head;
 	t_lst					*new_link;
 	t_worker				*new_worker;
 	t_dispatcher			*dispatcher;
@@ -29,8 +30,23 @@ void	*connect_worker_thread(void *param)
 		printf("worker connected fd: %d\n", fd);
 		new_link = calloc(1, sizeof(t_lst));
 		new_link->data = calloc(1, sizeof(t_worker));
-		new_link->next = dispatcher->workers;
-		dispatcher->workers = new_link;
+		new_link->next = NULL;
+		head = dispatcher->workers;
+		printf("f0\n");
+		if (head)
+		{
+			printf("head->next\n");
+			while (head->next)
+				head = head->next;
+			head = new_link;
+			head->next = NULL;
+		}
+		else
+		{
+			printf("new list\n");
+			dispatcher->workers = new_link;
+		}
+		printf("finished with connect worker\n");
 		new_worker = (t_worker *)new_link->data;
 		new_worker->socket.fd = fd;
 		new_worker->tid = 0;
@@ -39,7 +55,6 @@ void	*connect_worker_thread(void *param)
 			printf("worker accept call failed\n");
 			return (0);
 		}
-		dispatcher->is_connect = 0;
 	}
 	return (0);
 }
@@ -48,10 +63,7 @@ void  connect_workers(t_dispatcher *dispatcher, t_lst **workers)
 {
 	pthread_t	*worker_conn_thr;
 
-	printf("c0\n");
 	worker_conn_thr = (pthread_t *)calloc(1, sizeof(pthread_t));
-	printf("c1\n");
 	pthread_create(worker_conn_thr, NULL, connect_worker_thread, dispatcher);
-	printf("c2\n");
 	return ;
 }
--- networking/Dispatcher/launch_simulation.c
+++ networking/Dispatcher/launch_simulation.c
@@ -6,7 +6,7 @@
 /*   By: cyildiri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/11 20:53:00 by cyildiri          #+#    #+#             */
-/*   Updated: 2017/05/18 19:58:21 by ssmith           ###   ########.fr       */
+/*   Updated: 2017/05/18 23:40:42 by ssmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,7 @@ static void	make_new_event_thread(t_dispatcher *disp, t_lst *worker_link)
 static void	*handle_worker_connection(void *input)
 {
 	t_thread_handler	*params;
+	t_lst				*head;
 	t_lst				*worker;
 	t_worker			*cur_worker;
 	t_msg				msg;
@@ -51,17 +52,29 @@ static void	*handle_worker_connection(void *input)
 	send_worker_msg(cur_worker, new_message(WORK_UNITS_READY, 0, ""));
 	while (1)
 	{
-		if (worker->next && ((t_worker *)worker->next)->tid == 0)
+		printf("while 1\n");
+		head = params->dispatcher->workers;
+		while (head)
 		{
+			printf("worker: %d\n", ((t_worker*)(head->data))->socket.fd);
+			head = head->next;
+		}
+		if (worker->next && ((t_worker *)worker->next->data)->tid == 0)
+		{
+			printf("new event thread\n");
 			make_new_event_thread(params->dispatcher, worker->next);
 		}
 		msg = get_worker_msg(cur_worker);
 		printf("msg status: %d\n", msg.error);
 		if (msg.error == -1)
+		{
 			printf("get worker message failed with err %d\n", errno);
+			break ;
+		}
 		else if (msg.error == 0)
 		{
 			printf("worker connection terminated!\n");
+			close(cur_worker->socket.fd);
 			cur_worker->socket.fd = 0;
 			printf("attempting to reconnect...\n");
 			cur_worker->socket.fd = accept(params->dispatcher->sin.fd, (struct sockaddr *)&(params->dispatcher->sin.addr.sin_addr), &(params->dispatcher->sin.addrlen));
@@ -70,6 +83,7 @@ static void	*handle_worker_connection(void *input)
 		else
 			handle_worker_msg(params->dispatcher, cur_worker, msg);
 	}
+	return (0);
 }
 
 void		launch_simulation(t_dispatcher *dispatcher)
@@ -98,6 +112,7 @@ void		launch_simulation(t_dispatcher *dispatcher)
 	cur_worker->tid = calloc(1, sizeof(pthread_t));
 	printf("ALLOCCED\n");
 	pthread_create(cur_worker->tid, NULL, handle_worker_connection, param);
+	printf("sleeping\n");
 	sleep(999999);
 	printf("END\n");
 }
