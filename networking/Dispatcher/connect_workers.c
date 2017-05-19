9ba0c5ddae877e5ec71c468a3930da130e6becbd
Sean Smith <ssmith@student.42.us.org>
2017-05-18 17:40:29 -0700

    fixed

--- networking/Dispatcher/connect_workers.c
+++ networking/Dispatcher/connect_workers.c
@@ -6,7 +6,7 @@
 /*   By: scollet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/08 21:10:00 by scollet           #+#    #+#             */
-/*   Updated: 2017/05/18 16:52:03 by ssmith           ###   ########.fr       */
+/*   Updated: 2017/05/18 17:36:55 by ssmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ void	*connect_worker_thread(void *param)
 {
 	t_lst					*new_link;
 	t_worker				*new_worker;
-	t_socket				*sin;
 	t_dispatcher			*dispatcher;
 	struct sockaddr_storage	their_addr;
 	int						fd;
@@ -26,23 +25,15 @@ void	*connect_worker_thread(void *param)
 	dispatcher = (t_dispatcher *)param;
 	while (dispatcher->is_connect)
 	{
-		sin = calloc(1, sizeof(t_socket));
-		printf("1\n");
-		fd = accept(sin->fd, (struct sockaddr *)&(sin->addr.sin_addr), &(sin->addrlen));
+		fd = accept(dispatcher->sin.fd, (struct sockaddr *)&(dispatcher->sin.addr.sin_addr), &(dispatcher->sin.addrlen));
 		printf("worker connected fd: %d\n", fd);
 		new_link = calloc(1, sizeof(t_lst));
 		new_link->data = calloc(1, sizeof(t_worker));
-		printf("3\n");
 		new_link->next = dispatcher->workers;
-		printf("4\n");
 		dispatcher->workers = new_link;
-		printf("5\n");
 		new_worker = (t_worker *)new_link->data;
-		printf("6\n");
 		new_worker->socket.fd = fd;
-		printf("7\n");
 		new_worker->tid = 0;
-		printf("8\n");
 		if (new_worker->socket.fd == -1)
 		{
 			printf("worker accept call failed\n");
