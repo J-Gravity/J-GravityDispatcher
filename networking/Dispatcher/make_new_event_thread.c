cbe5b921b248bd3d6b7fd32d11504b4a7af993fe
Can Yildirim <cyildiri@student.42.us.org>
2017-05-22 17:09:30 -0700

    now opens n-number of tcp connections with no segfaults

--- networking/Dispatcher/make_new_event_thread.c
+++ networking/Dispatcher/make_new_event_thread.c
@@ -6,7 +6,7 @@
 /*   By: cyildiri <cyildiri@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/22 13:37:17 by cyildiri          #+#    #+#             */
-/*   Updated: 2017/05/22 14:09:11 by cyildiri         ###   ########.fr       */
+/*   Updated: 2017/05/22 17:00:15 by cyildiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	make_new_event_thread(t_dispatcher *disp, t_lst *worker_link)
 
 	params = new_thread_handler(disp, worker_link);
 	worker = (t_worker *)worker_link->data;
+	worker->tid = calloc(1, sizeof(pthread_t));
 	pthread_create(worker->tid, NULL,
 					handle_worker_connection, params);
 }
