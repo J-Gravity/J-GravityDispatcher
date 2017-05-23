20c0b3aa4e58dd28e642f77e0f908352041bb672
Phil MCLAUGHLIN <pmclaugh@e1z2r5p27.42.us.org>
2017-05-22 22:34:13 -0700

    quick fix

--- networking/Dispatcher/serialize_workunit.c
+++ networking/Dispatcher/serialize_workunit.c
@@ -12,29 +12,6 @@
 
 #include "dispatcher.h"
 
-t_msg	serialize_workunit(t_workunit *workunit)
-{
-	t_msg	msg;
-
-	msg.data = calloc(1, sizeof(char));
-	msg.data[0] = workunit->id;
-	msg.size = 4;
-	strbjoin(&msg, itob(workunit->localcount), sizeof(int));
-	for (int i = 0; i < workunit->localcount; i++)
-	{
-		strbjoin(&msg, clftob(workunit->local_bodies[i].position), sizeof(float) * 4);
-		strbjoin(&msg, clftob(workunit->local_bodies[i].velocity), sizeof(float) * 4);
-	}
-	strbjoin(&msg, itob(workunit->neighborcount), sizeof(int));
-	for (int i = 0; i < workunit->neighborcount; i++)
-	{
-		strbjoin(&msg, clftob(workunit->neighborhood[i].position), sizeof(float) * 4);
-		strbjoin(&msg, clftob(workunit->neighborhood[i].velocity), sizeof(float) * 4);
-	}
-	strbjoin(&msg, clftob(workunit->force_bias), sizeof(float) * 4);
-	return (msg);
-}
-
 int wu_size(t_workunit w)
 {
 	int total = 12; //id, localcount, neighborcount
