89cdcda4b3f08665a12188424c229021d8afa699
IAN WAGNER <dawag415@users.noreply.github.com>
2017-05-25 14:59:36 -0700

    fixed up leaks and protected

--- networking/Dispatcher/clear_work_units.c
+++ networking/Dispatcher/clear_work_units.c
@@ -16,14 +16,31 @@ void	clear_unit(t_lst **work_units)
 {	
 	if (work_units == 0 || *work_units == 0)
 		return ;
-	free(((t_workunit *)((*work_units)->data))->local_bodies);
-	((t_workunit *)((*work_units)->data))->local_bodies = 0;
-	free(((t_workunit *)((*work_units)->data))->neighborhood);
-	((t_workunit *)((*work_units)->data))->neighborhood = 0;
-	free((*work_units)->data);
-	(*work_units)->data = 0;
-	free(*work_units);
-	*work_units = 0;
+	if (((t_workunit *)((*work_units)->data))->local_bodies)
+	{
+		free(((t_workunit *)((*work_units)->data))->local_bodies);
+		((t_workunit *)((*work_units)->data))->local_bodies = 0;
+	}
+	if (((t_workunit *)((*work_units)->data))->neighborhood)
+	{
+		free(((t_workunit *)((*work_units)->data))->neighborhood);
+		((t_workunit *)((*work_units)->data))->neighborhood = 0;
+	}
+	if ((*work_units)->data)
+	{
+		free((*work_units)->data);
+		(*work_units)->data = 0;
+	}
+	if ((t_lst *)(*work_units)->next)
+	{
+		free((t_lst *)(*work_units)->next);
+		(*work_units)->next = 0;
+	}
+	if (*work_units)
+	{
+		free(*work_units);
+		*work_units = 0;
+	}
 }
 
 void		clear_work_units(t_lst **work_units)
