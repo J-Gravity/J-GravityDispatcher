2ac70c06e897c7dcb381a31aa9d4fc23eb9f1f4c
root <root@template-debian.42.us.org>
2017-05-27 20:48:09 -0700

    getting there

--- networking/Dispatcher/dispatcher.h
+++ networking/Dispatcher/dispatcher.h
@@ -33,8 +33,11 @@
 # include <netdb.h>
 # include <string.h>
 # include <errno.h>
-# include <OpenCL/opencl.h>
+# include <CL/opencl.h>
 # include <pthread.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <signal.h>
 
 typedef struct			s_lst
 {
