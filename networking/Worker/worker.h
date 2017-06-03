cf2a1060e8244a15044aa4d09f6eed8a4526dbf7
Can <cyildiri@student.42.us.org>
2017-06-03 13:27:18 -0700

    added the neccesary header file

--- networking/Worker/worker.h
+++ networking/Worker/worker.h
@@ -32,6 +32,7 @@
 # include <errno.h>
 # include <OpenCL/opencl.h>
 # include <pthread.h>
+# include <semaphore.h>
 
 #define G 1.327 * __exp10(13) //kilometers, solar masses, (km/s)^2
 #define SOFTENING 10000
