0008f2ee6aaddd6d06f30ecf9c6b7e4fcea6118c
Can Yildirim <cyildiri@student.42.us.org>
2017-06-02 18:57:14 -0700

    added calculation thread code

---
+++ networking/Worker/calc_thread.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calc_thread.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cyildiri <cyildiri@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/02 18:29:42 by cyildiri          #+#    #+#             */
+/*   Updated: 2017/06/02 18:52:38 by cyildiri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "worker"
+
+static void *calc_thread(void *param)
+{
+	t_worker	*worker;
+    t_workunit  *workunit;
+
+	worker = (t_worker *)param;
+    while (1)
+    {
+        if (worker->todo_work.count > 0)
+        {
+            workunit = queue_pop(&worker->todo_work);
+            workunit = do_workunit(workunit);
+            queue_enqueue(&worker->completed_work, queue_create_new(workunit));
+        }
+    }
+    return (0);
+}
+
+void launch_calculation_thread(t_worker *worker)
+{
+    pthread_create(worker->event_thread, NULL, calc_thread, worker);
+}
