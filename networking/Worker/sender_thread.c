6ea8a9810af2cad8d869e6b34954e9a9482d59df
Can Yildirim <cyildiri@student.42.us.org>
2017-08-10 11:19:03 -0700

    sender thread is now used for integrating workunits

--- networking/Worker/sender_thread.c
+++ networking/Worker/sender_thread.c
@@ -6,15 +6,16 @@
 /*   By: cyildiri <cyildiri@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/02 19:20:34 by cyildiri          #+#    #+#             */
-/*   Updated: 2017/06/10 14:52:48 by ssmith           ###   ########.fr       */
+/*   Updated: 2017/08/10 11:16:39 by cyildiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "worker.h"
 
-static void *sender_thread(void *param)
+static void *integration_thread(void *param)
 {
 	t_worker	*worker;
+	t_tree		*local_cell;
 	t_workunit	*workunit;
 	t_msg		msg;
 
@@ -24,25 +25,19 @@ static void *sender_thread(void *param)
 	    if (sem_wait(worker->sender_thread_sem) < 0)
 		    printf("SEND- sem_wait failed with err:%d\n", errno);
 		clock_t send_time = time(NULL);
-	   	if (DEBUG)
-            printf("SEND- sending work unit\n");
         workunit = queue_pop(&worker->completed_work);
-        //send complete work unit
-        msg = serialize_workunit(*workunit);
-		if (DEBUG)
-        	printf("sending: %d\n", workunit->id);
-		send_msg(worker->socket.fd, msg);
+		local_cell = dispatcher->cells[complete_WU.id];
+		integrate_WU_results(dispatcher, local_cell, workunit);
 		free(workunit->local_bodies);
 		free(workunit);
-	   	if (DEBUG)
-            printf("SEND- finished sending work unit\n");
+		dispatcher->workunits_done++;
 		G_total_send_time += time(NULL) - send_time;
     }
     return (0);
 }
 
-void launch_sender_thread(t_worker *worker)
+void launch_integration_thread(t_worker *worker)
 {
-	worker->sender_thread = (pthread_t *)calloc(1, sizeof(pthread_t));
-	pthread_create(worker->sender_thread, NULL, sender_thread, worker);
+	worker->integration_thread = (pthread_t *)calloc(1, sizeof(pthread_t));
+	pthread_create(worker->integration_thread, NULL, integration_thread, worker);
 }
